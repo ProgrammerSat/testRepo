9ab38bdbfb537ca642dbc9d43979afb3acbd4cbd
krappTeam <krconsultancy0107@gmail.com>
2025-08-02 10:57:19 +0530

    Final updates before Demo

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,13 +1,19 @@
 // middleware/upload.js
 const multer = require("multer");
 const { GridFsStorage } = require("multer-gridfs-storage");
+require("dotenv").config();
 
 const storage = new GridFsStorage({
   url: process.env.MONGO_URI,
-  file: (req, file) => ({
-    filename: file.originalname,
-    bucketName: "uploads",
-  }),
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      const fileInfo = {
+        filename: file.originalname,
+        bucketName: "uploads",
+      };
+      resolve(fileInfo);
+    });
+  },
 });
 
 const upload = multer({ storage });
